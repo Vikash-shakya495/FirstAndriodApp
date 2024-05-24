046482a7768054810bc39f178bc1a0d55aac7a83
Vikash-shakya495 <vikashshakya367@gmail.com>
2024-05-24 17:01:24 +0530

    Add Readme

---
+++ components/companyData.js
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native"
+const CompanyData = () => {
+    return(
+      <View>
+        <Text style={{fontSize: 20}}>Company Name: TCS</Text>
+        <Text style={{fontSize: 20}}>Total Emp: 47</Text>
+        <Text style={{fontSize: 20}}>Product: Instagram</Text>
+      </View>
+    ) 
+  };
+
+  export default CompanyData;
